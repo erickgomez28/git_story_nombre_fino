a73ae3b2d6d349c14b0e8fc13080c0f2b130a03b
gabriel216 <gabrielgomez353@gmail.com>
2017-03-21 09:25:29 -0400

    Add files via upload

---
+++ git_calc.rb
@@ -0,0 +1,27 @@
+
+def main
+	def mult(a,b)
+
+		return =a*b
+
+	end
+
+	def resta(a,b)
+
+		return a-b
+
+	end	
+
+
+	def suma( a, b )
+		return a+b
+	end	
+
+
+	def div(a,b)
+
+		return = a / b
+		
+	end
+end
+main
